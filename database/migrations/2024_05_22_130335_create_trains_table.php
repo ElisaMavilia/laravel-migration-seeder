09786329be7f398081c9503c620a0bcaccd20a3b
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-22 15:32:23 +0200

    added table fields

--- database/migrations/2024_05_22_130335_create_trains_table.php
+++ database/migrations/2024_05_22_130335_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time', '');
+            $table->string('arrival_time');
+            $table->string('train_code');
+            $table->bigInteger('carriages_number')->unsigned();
+            $table->tinyInteger('on_time');
+            $table->tinyInteger('cancelled');
             $table->timestamps();
         });
     }
