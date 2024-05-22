9db10661b0aad8a60af415f6c87ff1944f16ee72
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-22 18:11:35 +0200

    adjustments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,34 +6,28 @@
 <main class="container mt-4">
     <h1 class="pt-5 pb-3">Departures</h1>
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Departure</th>
-      <th scope="col">Arrival</th>
-      <th scope="col">Departure time</th>
-      <th scope="col">Arrival time</th>
-      <th scope="col">Train Number</th>
-      
+        <thead>
+            <tr>
+                <th scope="col">Departure</th>
+                <th scope="col">Arrival</th>
+                <th scope="col">Departure time</th>
+                <th scope="col">Arrival time</th>
+                <th scope="col">Train Number</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                    <tr class="table-dark table-striped">
+                        <td scope="row">{{$train->departure_station}}</td>
+                        <td>{{$train->arrival_station}}</td>
+                        <td class="text-danger">{{$train->departure_time}}</td>
+                        <td>{{$train->arrival_time}}</td>
+                        <td>{{$train->train_code}}</td>
 
-    </tr>
-  </thead>
-  
-  <tbody>
-  @foreach ($trains as $train)
-    <tr class="table-dark table-striped">
-      <td scope="row">{{$train->departure_station}}</td>
-      <td>{{$train->arrival_station}}</td>
-      <td>{{$train->departure_time}}</td>
-      <td>{{$train->arrival_time}}</td>
-      <td>{{$train->train_code}}</td>
-
-    </tr>
-   
-  </tbody>
-  @endforeach
-</table>
-     
-
+                    </tr>
+                </tbody>
+            @endforeach
+    </table>
     </div>
 </main>
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,9 @@
-<header>
-    <h4 class="bg-warning py-3 ps-3">Transport information</h4>
+<header class="d-flex align-content-center align-items-center bg-warning">
+    <div id="left-container">
+        <h4 class=" py-3 ps-4">Transport information</h4>
+    </div>
+    <div id="right-container">
+        <span><i class="fa-solid fa-train"></i></span>
+    </div>
+    
 </header>
