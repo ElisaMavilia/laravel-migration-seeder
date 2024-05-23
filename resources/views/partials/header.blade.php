df99222f1c42335b1329fd57ab11f468de4df8e9
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-23 15:38:11 +0200

    adj

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="d-flex align-content-center align-items-center bg-warning">
     <div id="left-container">
-        <h4 class=" py-3 ps-4">Transport information</h4>
+        <h4 class=" py-3 ps-4">Travel information</h4>
     </div>
     <div id="right-container">
         <span><i class="fa-solid fa-train"></i></span>
